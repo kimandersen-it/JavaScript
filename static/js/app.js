54651fdef9c7b27c27d10b861e87a857d059e9ea
Kimberly Andersen <44251377+kimandersen-it@users.noreply.github.com>
2019-03-06 17:59:28 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -167,38 +167,3 @@ UFOs(filteredShape);
 
  });
 
-//New Form TESTING
-
-
-submit.on("click", function() {
-
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#example-form-input");
-
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
-
-  console.log(inputValue);
-
-  // Set the span tag in the h1 element to the text
-  // that was entered in the form
-  d3.select("h1>span").text(inputValue);
-});
-
-submit.on("click", function() {
-
-var form = document.getElementById("form").form.id;
-  
-    // Prevent the page from refreshing
-  d3.event.preventDefault();
-
-  console.log(form.elements[0].type);
-  console.log(form.elements[1].type);
-  console.log(form.elements[2].type);
-
-  console.log(form.elements.name.form == form);
-
-});
